763a61ce385ca75aa054fe8086cce8daf2e825d3
unknown <harirajlimbu075@gmail.com>
2025-03-24 21:15:02 +0545

    project initialize

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+   <div>
+    Subscribe to HangCoder
+    <br/>
+    <Button variant='destructive'>Subscribe</Button>
+   </div>
+  );
+}
